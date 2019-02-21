774cd3f3d6e55a27f22155e84fd964e9ebaa4448
The-3D <frangellaemilio@gmail.com>
2019-02-21 17:28:10 +0100

    Added broadcasting support

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,9 @@ const token = '';
 
 const bot = new TelegramBot(token, { polling: true });
 
-const marketplace = new Marketplace('','https://api.aave.com');
+const marketplace = new Marketplace('', 'https://api.aave.com');
 
-let latestLoansList : string[] | null = null;
+let latestLoansList: string[] | null = null;
 
 
 const formatLoanData = (data: LoanRequestModel) => {
@@ -20,15 +20,34 @@ const formatLoanData = (data: LoanRequestModel) => {
   message += `collateral:<b> ${data.collateralAmount} ${data.collateralType} </b> \n`;
   message += `loan amount:<b> ${data.loanAmount} ${data.moe} </b> \n`;
   message += `Monthly interest: <b>${data.mpr}%</b> \n`;
-  message += `Duration: <b>${data.duration*30} days</b> \n \n \n`;
+  message += `Duration: <b>${data.duration * 30} days</b> \n \n \n`;
 
   message += `<b>Fund this request and earn interest on </b> <a href="ethlend.io">https://ethlend.io</a> \n \n \n`;
 
   return message;
 }
 
+
+const fetchAvailableLoans = async (): Promise<LoanRequestModel[]> => {
+
+  const allRequestsAddresses = await marketplace.requests.getAllAddresses();
+
+  let loans = [];
+
+  for (let address of allRequestsAddresses.reverse()) {
+
+    let loanData = await marketplace.requests.getLoanData(address);
+
+    if (loanData.state && loanData.state.toLowerCase() === "funding") {
+      loans.push(loanData);
+    }
+
+  }
+  return loans;
+}
+
 bot.onText(/\/whoisthelord/, (msg: any, match: any) => {
- 
+
   let message = "I'm the lords of the loans, built with the AAVE SDK, and i rule over the ETHLend world. \n";
   message += "If you want to become my subject, write to me in private and type <b>/register</b>. You shall receive notifications when i consider it appropriate.";
   message += " Type <b>/requests</b> if you want to know which loans are available on the platform to fund.";
@@ -58,6 +77,48 @@ bot.onText(/\/register/, async (msg: any, match: any) => {
 
 });
 
+bot.onText(/\/disablebroadcast/, async (msg: any, match: any) => {
+
+  const chatId = msg.chat.id;
+
+  const adapter = new FileAsync('data.json')
+  const db = await lowdb(adapter);
+
+  let chatData = db.get("broadcasting").find(item => item.chatId === chatId).value();
+
+  if (chatData) {
+
+    db.get("broadcasting").remove((item: any) => item.chatId === chatId).write().then(() => {
+      bot.sendMessage(msg.chat.id, "Broadcasting disabled for this channel.");
+
+    });
+  }
+  else
+    bot.sendMessage(msg.chat.id, "Channel is not registered for broadcasting.");
+
+
+});
+
+bot.onText(/\/enablebroadcast/, async (msg: any, match: any) => {
+
+  const chatId = msg.chat.id;
+
+  const adapter = new FileAsync('data.json')
+  const db = await lowdb(adapter);
+
+  if (db.get("broadcasting").find(item => item.chatId === chatId).value()) {
+    bot.sendMessage(msg.chat.id, "This channel is already configured to receive information about the loan.");
+    return;
+  }
+
+  db.get("broadcasting").push({ chatId: chatId }).write().then(() => {
+
+    bot.sendMessage(msg.chat.id, "Broadcasting of the loans enabled for this channel.");
+
+  });
+
+});
+
 
 bot.onText(/\/unregister/, async (msg: any, match: any) => {
 
@@ -69,7 +130,7 @@ bot.onText(/\/unregister/, async (msg: any, match: any) => {
   let chatData = db.get("registrations").find(item => item.chatId === chatId).value();
 
 
-  
+
   if (chatData) {
 
     db.get("registrations").remove((item: any) => item.chatId === chatId).write().then(() => {
@@ -90,27 +151,16 @@ bot.onText(/\/requests/, async (msg: any, match: any) => {
 
   try {
 
-    const allRequestsAddresses = await marketplace.requests.getAllAddresses();
 
-    let loansFound = 0;
+    let loans = await fetchAvailableLoans();
 
-
-    for (let address of allRequestsAddresses.reverse()) {
-
-      let loanData = await marketplace.requests.getLoanData(address);
-    
-      if(loanData.state && loanData.state.toLowerCase() === "funding"){
-
-        let message = formatLoanData(loanData);
-        bot.sendMessage(chatId, message, { parse_mode: "HTML" });
-        loansFound++;
-      }
-
-
+    if (loans.length === 0) {
+      bot.sendMessage(chatId, "All loans are funded right now.");
+      return;
     }
 
-    if(loansFound == 0){
-       bot.sendMessage(chatId, "All loans are funded right now.");
+    for (let loan of loans) {
+      bot.sendMessage(chatId, formatLoanData(loan), { parse_mode: "HTML" });
     }
 
   } catch (e) {
@@ -128,32 +178,65 @@ setInterval(async () => {
 
 
   const currentList = await marketplace.requests.getAllAddresses();
-  
-  if(!latestLoansList){
+
+  if (!latestLoansList) {
     latestLoansList = currentList;
     return;
   }
 
-  if(currentList.length > latestLoansList.length){
-    
-    let difference =  currentList.filter( address =>  latestLoansList && !latestLoansList.includes(address));
-    
+  if (currentList.length > latestLoansList.length) {
+
+    let difference = currentList.filter(address => latestLoansList && !latestLoansList.includes(address));
+
     for (let address of difference) {
 
       let loanData = await marketplace.requests.getLoanData(address);
       let message = "<b>NEW LOAN REQUEST</b> \n \n" + formatLoanData(loanData);
 
-      db.get("registrations").value().forEach( (item : any) => {
+      db.get("registrations").value().forEach((item: any) => {
 
-        bot.sendMessage(item.chatId, message, { parse_mode: "HTML" });
- 
+        try {
+          bot.sendMessage(item.chatId, message, { parse_mode: "HTML" });
+        }
+        catch (e) {
+          console.log("Exception while sending message to chat ", item.chatId, ", reason: ", e);
+        }
+
       });
 
     }
 
     latestLoansList = currentList;
-    
+
   }
 
 
 }, 120000);
+
+
+setInterval(async () => {
+
+  const adapter = new FileAsync('data.json')
+  const db = await lowdb(adapter);
+
+  let loans = await fetchAvailableLoans();
+
+
+  db.get("broadcasting").value().forEach((item: any) => {
+
+    bot.sendMessage(item.chatId, "<b>AVAILABLE LOAN REQUESTS</b>", { parse_mode: "HTML" });
+
+    for (let loan of loans) {
+      try {
+        bot.sendMessage(item.chatId, formatLoanData(loan), { parse_mode: "HTML" });
+      }
+      catch (e) {
+        console.log("Exception while sending message to chat ", item.chatId, ", reason: ", e);
+      }
+    }
+
+  });
+
+
+}, 1200000);
+
